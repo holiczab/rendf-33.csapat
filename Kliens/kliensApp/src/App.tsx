b455f8640c43f2bf958b0be5982dbd4f04629677
oldman1996 <oldman1996@gmail.com>
2022-03-22 13:21:02 +0100

    React-TS intial template created

---
+++ Kliens/kliensApp/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">Rendszerfejlesztés - 33. csapat</div>
+  );
+}
+
+export default App;
