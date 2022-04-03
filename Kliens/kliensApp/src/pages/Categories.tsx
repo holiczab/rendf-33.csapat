7860bfd15ef6ed8f7d02e963d26e9c71cdcd409b
molnard888 <76199536+molnard888@users.noreply.github.com>
2022-04-03 09:51:45 +0200

    'Caterories' table with  DB data

--- Kliens/kliensApp/src/pages/Categories.tsx
+++ Kliens/kliensApp/src/pages/Categories.tsx
@@ -7,9 +7,33 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
+import { w3cwebsocket as W3CWebSocket } from "websocket";
+import { SelectAllSharp } from '@mui/icons-material';
+import { StyledEngineProvider } from '@mui/styled-engine-sc';
 
+const client = new W3CWebSocket("ws://127.0.0.1:5050");
 
+var rows: Data[];
+  
+  //createData('India', 'IN', '1324171354', '3287263', 'China', 'CN', 'Italy')
 
+rows = [
+  createData('', '', '', '', '', '', '')
+];
+
+/*function sleep(ms: number | undefined) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}*/
+
+function FetchDataFromDB(){
+  //rows = [];
+  var mess = "scat";
+  client.send(mess);
+  console.log(mess);
+  //await sleep(2000);
+};
+
+FetchDataFromDB();
 
 interface Column {
   id: 'ID' | 'Name' | 'ParentID' | 'Interval' | 'Specification' | 'StandardTime' | 'RequredQualification';
@@ -81,77 +105,87 @@ function createData(
   return { ID, Name, ParentID, Interval, Specification, StandardTime, RequredQualification };
 }
 
-var rows: Data[];
-  
-  //createData('Brazil', 'BR', 210147125, 8515767),
-
-rows = [
-  createData('India', 'IN', '1324171354', '3287263', 'China', 'CN', 'Italy')
-  
-];
-
-function FetchDataFromDB(){
-
-};
-
 
 
 
 function Categories() {
-  const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+  React.useEffect(
+    // HA sikerese a kapcsolat, és HA üzenet érkezik a szervertől
+    () => {
+      client.onopen = () => {
+        console.log("WebSocket Client Connected");
+      };
+      client.onmessage = (message: any) => {
+        //console.log(message.data);
+        
+        rows = [];
+        var SplittedMessage = message.data.split("\n");
+        for (let Row in SplittedMessage){
+          console.log(SplittedMessage[Row]);
+          var SplittedRow = SplittedMessage[Row].split(";");
+          for (let str in SplittedRow){
+            if (SplittedRow[str] === "None"){
+              str = "";
+            }
+          }  
+          rows.push(createData(SplittedRow[0], SplittedRow[1], SplittedRow[2], SplittedRow[3], SplittedRow[4], SplittedRow[5], SplittedRow[6]));
+        }
+        
+        for (let entry of rows) {
+          console.log(entry); 
+        }
+        
+        TableReturn();
+      };
+    },
+    []
+  );
 
-  const handleChangePage = (event: unknown, newPage: number) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setRowsPerPage(+event.target.value);
-    setPage(0);
-  };
+  function TableReturn(){
+    return <main style={{paddingLeft: 280}}>
+    <Paper sx={{ width: '95%', overflow: 'hidden' }}>
+    <TableContainer sx={{ maxHeight: 440 }}>
+      <Table stickyHeader aria-label="sticky table">
+        <TableHead>
+          <TableRow>
+            {columns.map((column) => (
+              <TableCell
+                key={column.id}
+                align={column.align}
+                style={{ minWidth: column.minWidth }}
+              >
+                {column.label}
+              </TableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows
+            .map((row) => {
+              return (
+                <TableRow hover role="checkbox" tabIndex={-1} key={row.ID}>
+                  {columns.map((column) => {
+                    const value = row[column.id];
+                    return (
+                      <TableCell key={column.id} align={column.align}>
+                        {column.format && typeof value === 'number'
+                          ? column.format(value)
+                          : value}
+                      </TableCell>
+                    );
+                  })}
+                </TableRow>
+              );
+            })}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  </Paper>
+  </main>
+  }
 
   return (
-    <main style={{paddingLeft: 280}}>
-      <Paper sx={{ width: '95%', overflow: 'hidden' }}>
-      <TableContainer sx={{ maxHeight: 440 }}>
-        <Table stickyHeader aria-label="sticky table">
-          <TableHead>
-            <TableRow>
-              {columns.map((column) => (
-                <TableCell
-                  key={column.id}
-                  align={column.align}
-                  style={{ minWidth: column.minWidth }}
-                >
-                  {column.label}
-                </TableCell>
-              ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows
-              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              .map((row) => {
-                return (
-                  <TableRow hover role="checkbox" tabIndex={-1} key={row.ID}>
-                    {columns.map((column) => {
-                      const value = row[column.id];
-                      return (
-                        <TableCell key={column.id} align={column.align}>
-                          {column.format && typeof value === 'number'
-                            ? column.format(value)
-                            : value}
-                        </TableCell>
-                      );
-                    })}
-                  </TableRow>
-                );
-              })}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </Paper>
-    </main>
+    TableReturn()
   );
 }
 
