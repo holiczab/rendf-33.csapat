0a3d4448064e95864295b9f4c7d3050e3453dccc
oldman1996 <oldman1996@gmail.com>
2022-05-13 09:32:01 +0200

    Fixed select_maintenancetask
    Finished tasks will not be sended to client

--- Szerver/database.py
+++ Szerver/database.py
@@ -300,11 +300,20 @@ class DataBase:
         self.conn.close()
     
     def select_maintenancetask(self):
+        localtime = time.localtime()
+        date = time.strftime("%m/%d/%Y", localtime)
+
         cursor=self.conn.execute("SELECT * FROM MaintenanceTasks INNER JOIN Log ON Log.Task=MaintenanceTasks.ID WHERE Status != 'Finished' ")
         result=cursor.fetchall()
         msg=""
         for row in result:
-            msg+=str(row[0])+";"+str(row[1])+";"+str(row[2])+";"+str(row[3])+";"+str(row[4])+";"+str(row[5])+";"+str(row[6])+";"+self.get_ID_SpecName(str(row[8]))+";"+self.get_ID_SpecName(str(row[10]))+";"+str(row[12])+"END_OF_ROW"
+            if row[12] is None:
+                msg+=str(row[0])+";"+str(row[1])+";"+str(row[2])+";"+str(row[3])+";"+str(row[4])+";"+str(row[5])+";"+str(row[6])+";"+self.get_ID_SpecName(str(row[8]))+";"+self.get_ID_SpecName(str(row[10]))+";"+str(row[12])+"END_OF_ROW"
+            else:
+                end=datetime(int(row[12].split("/")[2]),int(row[12].split("/")[0]),int(row[12].split("/")[1]))
+                today=datetime(int(date.split("/")[2]),int(date.split("/")[0]),int(date.split("/")[1]))
+                if self.time_to_int(end) >= self.time_to_int(today):
+                    msg+=str(row[0])+";"+str(row[1])+";"+str(row[2])+";"+str(row[3])+";"+str(row[4])+";"+str(row[5])+";"+str(row[6])+";"+self.get_ID_SpecName(str(row[8]))+";"+self.get_ID_SpecName(str(row[10]))+";"+str(row[12])+"END_OF_ROW"
         print("Selected_MaintenanceTasks completed!")
         return msg
 
